681ae443af59616948fb480fdb3c9c2cde4b7b00
Osky-71 <140463298+Osky-71@users.noreply.github.com>
2023-08-11 12:02:02 -0600

    Add files via upload

---
+++ P1/js/index.js
@@ -0,0 +1,29 @@
+/*let edad = prompt("Introduzca su edad")
+if(edad>18 && edad<=100){
+alert("El usuario es mayor de edad")
+}
+else if(edad<18 && edad>0){
+    alert("El usuario es menor de edad")
+}
+else if(edad==18){
+    alert("El usuario tiene 18 años")
+}
+else if(edad > 100){
+    alert("El usuario es centenario")
+}
+else if(edad<=0){
+    alert("Edad invalida")
+}
+
+let i=1;
+let suma=0;
+while(1<=100){
+    suma +=i;
+    i++;
+}
+alert (suma)*/
+
+let num = prompt("Escriba un numero")
+for(i=1; i<=10; i++){
+    alert(num + " por " + i +" es igual a: "+(num*i))
+}
